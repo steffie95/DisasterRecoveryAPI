719c7b009c1d6eb3e36999a2c46317746e7a274e
jaysmore <43677365+jaysmore@users.noreply.github.com>
2020-02-27 14:27:08 -0500

    Update Jobs.cs

--- Models/Jobs.cs
+++ Models/Jobs.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,7 @@ namespace DisasterRecoveryAPI.Models
     public class Jobs
     {
         // Job id
+        [Key]
         public int Id { get; set; }
         // Job code
         public string JobCode { get; set; }
@@ -17,6 +19,8 @@ namespace DisasterRecoveryAPI.Models
         public int HrRate { get; set; }
         // Max Hours Per day
         public int MaxHrPerDay { get; set; }
+        
         public Timecard Timecard { get; set; }
+        public ICollection<Jobs_Timecard> Jobs_Timecard { get; set; }
     }
 }
