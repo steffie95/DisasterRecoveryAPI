85856494210a18ce95afceabcfe62e670b7f8226
jaysmore <43677365+jaysmore@users.noreply.github.com>
2020-02-27 14:26:53 -0500

    Update Timecard.cs

--- Models/Timecard.cs
+++ Models/Timecard.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,14 +9,20 @@ namespace DisasterRecoveryAPI.Models
     public class Timecard
     {
         // Timecard ID
+        [Key]
         public int Id { get; set; }
+        public string SiteCode { get; set; }
         // Contractor name
         public string Name { get; set; }
         //Date created
         public DateTime DateCreated { get; set; }
+        public int TotalJobHrs { get; set; }
+        public int TotalMachineHrs { get; set; }
+        public int TotalJobAmount { get; set; }
+        public int TotalMachineAmount { get; set; }
         //TODO: create isdone
         public Boolean isConfirmed { get; set; }
-        public ICollection<Jobs> Jobs { get; set; }
-        public ICollection<Machines> Machines { get; set; }
+        public ICollection<Jobs_Timecard> Jobs_Timecards { get; set; }
+        public ICollection<Machines_Timecard> Machines_Timecards { get; set; }
     }
 }
