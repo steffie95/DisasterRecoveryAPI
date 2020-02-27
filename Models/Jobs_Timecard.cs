6113dc2cee8d285731a8637503d9600d33dbfee6
jaysmore <43677365+jaysmore@users.noreply.github.com>
2020-02-27 14:28:06 -0500

    Add files via upload

---
+++ Models/Jobs_Timecard.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DisasterRecoveryAPI.Models
+{
+    public class Jobs_Timecard
+    {
+        [Key]
+        public int Id { get; set; }
+        public int JobId { get; set; }
+        public int TimecardId { get; set; }
+        public Jobs Jobs { get; set; }
+        public Timecard Timecard{get;set;}
+    }
+}
