fa8497b96894b9a6620ddd9bf0f7b7f168b7cf0f
Stefanie Adrianzen <stefanie.adrianzen@gmail.com>
2020-02-28 09:51:42 -0500

    Added Role to JWT token.

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -63,7 +63,8 @@ namespace DisasterRecoveryAPI.Controllers
             var claims = new[]
             {
             new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
-            new Claim(ClaimTypes.Name, userFromRepo.Username)
+            new Claim(ClaimTypes.Name, userFromRepo.Username),
+            new Claim(ClaimTypes.Role, userFromRepo.Role.ToUpper())
             };
 
             var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config.GetSection("AppSettings:Token").Value));
